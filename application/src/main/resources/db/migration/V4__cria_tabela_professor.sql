eaaf4c86c6b7de82e93cf12f9d1922b2bb35855e
Taynan-Vieira <taynan.vieira.one@gmail.com>
2020-02-06 15:17:37 -0300

    #8- Foi feita uma alteração de local da coluna examinador no script de migracao

--- application/src/main/resources/db/migration/V4__cria_tabela_professor.sql
+++ application/src/main/resources/db/migration/V4__cria_tabela_professor.sql
@@ -4,7 +4,6 @@ create table if not exists public.tcc05_professor
     tcc05_nome varchar(255),
     tcc05_matricula varchar(255),
     tcc05_senha varchar(255),
-    tcc05_examinador boolean,
 
     constraint pktcc05_cod_professor primary key (tcc05_cod_professor)
 );
