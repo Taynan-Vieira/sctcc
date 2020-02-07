9ac3ed9d31c0cdea74cd75891b51241f69ee08ef
Taynan-Vieira <taynan.vieira.one@gmail.com>
2020-02-07 15:27:46 -0300

    #8- Insercao da coluna status na tabela trabalho e exclusao da coluna nota da mesma

--- application/src/main/resources/db/migration/V3__cria_tabela_trabalho.sql
+++ application/src/main/resources/db/migration/V3__cria_tabela_trabalho.sql
@@ -2,7 +2,7 @@ create table if not exists public.tcc03_trabalho
 (
     tcc03_cod_trabalho bigint not null,
     tcc03_tema varchar(255),
-    tcc03_nota bigint not null,
+    tcc03_status varchar(255),
     fktcc03tcc02_cod_aluno bigint not null,
 
     constraint pktcc03_cod_trabalho primary key (tcc03_cod_trabalho),
